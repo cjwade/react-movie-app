dc002e767804ac45880f2db42d2d8221b7d038b0
cjwade <89665017+cjwade@users.noreply.github.com>
2022-11-16 12:30:27 -0800

    Displayed movies on homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,25 +1,47 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import MovieCard from "../Components/MovieCard";
 import NavBar from "../Components/NavBar";
 import loading_spinner from "./../assets/loading_spinner.gif";
 
 const HomePage = () => {
-
 	const [movies, setMovies] = useState("Loading");
-   
 
-	if (movies === "Loading")
+	async function fetchMovies() {
+		const res = await axios.get(
+			`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.REACT_APP_MOVIE_API_KEY}`
+		);
+		console.log(res.data.results);
+		return res.data.results;
+	}
+
+	useEffect(() => {
+		fetchMovies()
+			.then((res) => {
+				setMovies(res);
+			})
+			.catch((err) => {
+				alert(err);
+			});
+	}, []);
+
+	if (movies === "Loading" || !movies || movies.length === 0)
 		return (
 			<div className="flex items-center justify-center h-screen bg-gray-700">
 				<img src={loading_spinner} alt="loading" height="200px" width="200px" />
 			</div>
 		);
-
-
-	return (
-		<div className="bg-gray-700 min-h-screen ">
-			<NavBar />
-		</div>
-	);
+	else
+		return (
+			<div className="bg-gray-700 min-h-screen ">
+				<NavBar />
+				<div className="flex flex-wrap justify-evenly">
+					{movies.map((movie) => (
+						<MovieCard movie={movie} />
+					))}
+				</div>
+			</div>
+		);
 };
 
 export default HomePage;
